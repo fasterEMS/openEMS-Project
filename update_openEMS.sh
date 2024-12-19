5fa547cb803a2b61ab0346cf2a21b09c6a6b9be1
Yifeng Li <tomli@tomli.me>
2024-12-19 20:54:00 +0000

    update_openEMS.sh: don't use "--user" for Python venv.
    When Python is running inside a Python venv prefix, adding the
    option "--user" causes module installation into the global system
    directory. Skip the option "--user" if we're running inside a
    venv.
    
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- update_openEMS.sh
+++ update_openEMS.sh
@@ -155,8 +155,11 @@ fi
 #####  python extension build ####
 
 if [ $BUILD_PY_EXT -eq 1 ]; then
+    # are we running inside a Python venv?
+    PY_INST_IS_VENV=$(python3 -c "import sys; exit(sys.prefix == sys.base_prefix)")
+
     PY_INST_USER=''
-    if (( $EUID != 0 )); then
+    if (( $EUID != 0 )) && (( $PY_INST_VENV != 1 )); then
         PY_INST_USER='--user'
     fi
     for PY_EXT in 'CSXCAD' 'openEMS'
