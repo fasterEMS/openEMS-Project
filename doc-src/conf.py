3d9f308be5df36de43e6e6c18b3adb6da6129e77
Yifeng Li <tomli@tomli.me>
2025-04-05 06:25:22 +0000

    doc-src/conf.py: use sphinxcontrib.video to include videos.
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'numpydoc',
     'sphinx.ext.autosummary',
+    'sphinxcontrib.video'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
